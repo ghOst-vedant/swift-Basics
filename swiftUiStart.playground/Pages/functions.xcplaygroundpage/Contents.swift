116be9deefda311f5127167fefacabb5a04bb38d
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-10-13 17:29:48 +0530

    [update]: Function in swift

--- swiftUiStart.playground/Pages/functions.xcplaygroundpage/Contents.swift
+++ swiftUiStart.playground/Pages/functions.xcplaygroundpage/Contents.swift
@@ -4,7 +4,36 @@ import Foundation
 
 var greeting:String = "Hello, playground"
 
-var myItem:Bool = true
+func greet(name:String) -> String {
+    return "Hello, " + name + "."
+}
 
+greet(name:"vedant")
+
+// Functions with multiple parameters
+
+func multiParameter(base:Int,power:Int) -> Int {
+    var num = 1
+    for _ in 0..<power{
+        num *= base
+    }
+    return num
+}
+multiParameter(base: 2, power: 10)
+
+//to remove argument names at the function call
+
+func calcMinus(_ val1:Int)->Int{
+    return val1 - 1
+}
+let res = calcMinus(10)
+print(res)
+
+//
+@discardableResult
+func customAdd(_ val1:Int,_ val2:Int) ->Int{
+    return val1+val2
+}
+print(customAdd(10, 20))
 //: [Next](@next)
 
