efdd5ef54fb4eb15228815f261643b07902acdf2
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-10-13 18:34:10 +0530

    [Update]: Enumerations in swift

--- enumerations.playground/Contents.swift
+++ enumerations.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+enum animals {
+case cat,dog,turtle
+}
+
+
+let cat = animals.turtle
+
+print(cat)
+
+//if cat == animals.cat{
+//   "This is a cat"
+//} else if cat == .dog{
+//    "This is a dog"
+//}else {
+//    "I dont know..."
+//}
+
+switch cat{
+case .cat:
+    "This is a cat"
+    break
+case .dog:
+    "This is a dog"
+    break
+case .turtle:
+    "This is a turtle"
+    break
+}
